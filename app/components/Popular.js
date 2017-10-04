8875f212fbaa4f6525423b55f52ec8d351ccb8df
tenwiz <nethe0002@gmail.com>
2017-10-04 16:27:39 +0800

    feat: stateless functional component & add bind

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 
+function SelectLanguage (props) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+  return (
+    <ul className='languages'>
+      {languages.map(lang => {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={() => props.onSelect(lang)}
+            key={lang}
+          >
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
 class Popular extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       selectedLanguage: 'All',
     }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
   }
 
   updateLanguage(lang) {
@@ -13,22 +35,13 @@ class Popular extends React.Component {
   }
 
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-
     return (
-      <ul className='languages'>
-        {languages.map(lang => {
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null}
-              onClick={() => this.updateLanguage(lang)}
-              key={lang}
-            >
-              {lang}
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     )
   }
 }
