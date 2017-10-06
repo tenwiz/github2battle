f06b3428e7fd51938370d3a8ce037af4c887ad5a
tenwiz <nethe0002@gmail.com>
2017-10-06 14:20:13 +0800

    feat: battle api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,64 @@
 import axios from 'axios'
 
+// const id = 'YOUR_CLIENT_ID'
+// const sec = 'YOUR_SECRET_ID'
+// const params = `?client_id=${id}&client_secret=${sec}`
+
+const getProfile = (username) => {
+  // return axios.get(`https://api.github.com/users/${username}${params}`)
+  return axios.get(`https://api.github.com/users/${username}`)
+    .then(user => user.data)
+}
+
+const getRepos = (username) => {
+  // return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+  return axios.get(`https://api.github.com/users/${username}/repos&per_page=100`)
+    .then(user => user.data)
+}
+
+const getStarCount = (repos) => {
+  return repos.reduce((count, repo) => {
+    return count + repo.stargazers_count
+  }, 0)
+}
+
+const calculateScore = (profile, repos) => {
+  const followers = profile.followers
+  const totalStars = getStarCount(repos)
+
+  return (followers * 3) + totalStars
+}
+
+const getUserData = (player) => {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(data => {
+    const profile = data[0]
+    const repos = data[1]
+
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+const sortPlayers = (players) => {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+const handleError = (error) => {
+  console.warn(error)
+  return null
+}
+
+export const battle = (players) => {
+  return axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError)
+}
+
 export const fetchPopularRepos = (language) => {
   const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
